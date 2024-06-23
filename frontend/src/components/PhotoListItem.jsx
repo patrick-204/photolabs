e093fdb957f86d44487a117ca5b8ac51254b89ea
patrick-204 <p.ohara921@gmail.com>
2024-06-23 14:36:10 -0400

    Modify photlistitem styling to fit the BEM naming convention.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,13 +8,13 @@ const PhotoListItem = (props) => {
     <div className="photo-list__item">
       <img src={props.photo.imageSource} className="photo-list__image" />
 
-      <img src={props.photo.profile} className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={props.photo.profile} className="photo-list__user-profile" />
+        <span className="photo-list__user-info ">{props.photo.username}</span>
+      </div>
 
-      <div className="photo-list__user-profile">
-        <div className="photo-list__user-detail">
-          <span className="photo-list__user-details">{props.photo.username}</span>
-        </div>
-        <span className="photo-list__user-location">{props.photo.location.city}</span>, <span className="country">{props.photo.location.country}</span>
+      <div className="photo-list__user-location">
+        <span>{props.photo.location.city}</span>, <span>{props.photo.location.country}</span>
       </div>
     </div>
   );
