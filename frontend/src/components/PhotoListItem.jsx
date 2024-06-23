f0fe989498d656b5c51bcabc481e6aab78c47178
patrick-204 <p.ohara921@gmail.com>
2024-06-23 14:25:37 -0400

    Modify styling to the PhotoListItem component.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,16 +5,16 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
 
   return (
-    <div className="App">
-      <div className="photo-details">
-        <img src={props.photo.imageSource} />
-      </div>
-      <div className="user-profile">
-        <img src={props.photo.profile} />
-        <span>{props.photo.username}</span>
-        <div className="location">
-          <span>{props.photo.location.city}</span>, <span className="country">{props.photo.location.country}</span>
+    <div className="photo-list__item">
+      <img src={props.photo.imageSource} className="photo-list__image" />
+
+      <img src={props.photo.profile} className="photo-list__image" />
+
+      <div className="photo-list__user-profile">
+        <div className="photo-list__user-detail">
+          <span className="photo-list__user-details">{props.photo.username}</span>
         </div>
+        <span className="photo-list__user-location">{props.photo.location.city}</span>, <span className="country">{props.photo.location.country}</span>
       </div>
     </div>
   );
