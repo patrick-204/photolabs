7471b3cbb71551461ded79c950f61aba68cffbfa
patrick-204 <p.ohara921@gmail.com>
2024-06-23 22:13:04 -0400

    Remove sampleDataForPhotoListItem from App component and comment out Array of PhotoListItem.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,14 @@ import PhotoListItem from './components/PhotoListItem';
 import PhotoFavButton from 'components/PhotoFavButton';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   // Array helper method
-  const photos = Array.from({ length: 3 }, (_, index) => (
-    <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
-  ));
+  // const photos = Array.from({ length: 3 }, (_, index) => (
+  //   <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
+  // ));
+
+  <PhotoList />
 
   return (
     <div className="App">
