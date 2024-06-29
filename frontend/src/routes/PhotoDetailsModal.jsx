2cce15a338911e6dc1b93a8d80a678e6d28d76ab
patrick-204 <p.ohara921@gmail.com>
2024-06-28 23:08:21 -0400

    Update the like icon and location. Update the photo styling to include user profile.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -15,27 +15,34 @@ const PhotoDetailsModal = (props) => {
     toggleFavourite(photo.id);
   };
 
-  return (
+    return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      
-      <img src={photo.urls.full} alt={photo.alt_description} className="photo-details-modal__image" />
-      <PhotoFavButton favourite={isFavourite} handleClick={handleFavouriteClick} />
-      
-      <div className="photo-details-modal__header">
-        <h2>{photo.description}</h2>
-        <div className="photo-details-modal__photographer-details">
-          <img
-            src={photo.user.profile}
-            alt={`${photo.user.name}'s profile`}
-            className="photo-details-modal__photographer-profile"
-          />
-          <div className="photo-details-modal__photographer-info">
-            <div>{photo.user.name}</div>
-            <div className="photo-details-modal__photographer-location">
-              {photo.location.city}, {photo.location.country}
+
+      <div className="photo-details-modal__content">
+        <div className="photo-details-modal__left">
+          <PhotoFavButton favourite={isFavourite} handleClick={handleFavouriteClick} />
+        </div>
+
+        <div className="photo-details-modal__right">
+          <img src={photo.urls.full} alt={photo.alt_description} className="photo-details-modal__image" />
+
+          <div className="photo-details-modal__header">
+            <h2>{photo.description}</h2>
+            <div className="photo-details-modal__photographer-details">
+              <img
+                src={photo.user.profile}
+                alt={`${photo.user.name}'s profile`}
+                className="photo-details-modal__photographer-profile"
+              />
+              <div className="photo-details-modal__photographer-info">
+                <div>{photo.user.name}</div>
+                <div className="photo-details-modal__photographer-location">
+                  {photo.location.city}, {photo.location.country}
+                </div>
+              </div>
             </div>
           </div>
         </div>
