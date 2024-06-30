52757a53435b930e7de481f947660cadeb6ff47a
patrick-204 <p.ohara921@gmail.com>
2024-06-30 17:56:23 -0400

    Modify PhotoFavButton component so that FavIcon component appears in top left of photos and is correctly positioned.

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,16 +1,13 @@
-import React, { useCallback, useState } from 'react';
-
+import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        <button onClick={props.handleClick}>
-          <FavIcon selected={props.favourite} />
-        </button>
-      </div>
+      <button className="fav-button" onClick={props.handleClick}>
+        <FavIcon selected={props.favourite} />
+      </button>
     </div>
   );
 }
