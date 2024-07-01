8ee6528a32610b76a1e6e4c8565112f481c819db
patrick-204 <p.ohara921@gmail.com>
2024-07-01 18:49:59 -0400

    Add some search functionality to the nav.

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss'
+import axios from 'axios';
 
 const TopNavigation = (props) => {
   const { topics, favouritesCount, onTopicClick } = props;
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleTopicClick = (topicId) => {
     onTopicClick(topicId); 
   };
 
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`/api/photos?search=${searchTerm}`);
+      console.log('Searcg results:', response.data);
+    } catch (error) {
+      console.error('Error finding photos:', error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    handleSearch();
+  };
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <div className="top-nav-bar__search">
+        <input
+        type="text"
+        placeholder="Search Photos..."
+        value={searchTerm}
+        onChange={handleChange}
+        />
+        <button onClick={handleButtonClick}>Search</button>
+      </div>
       <TopicList topics={topics} onTopicClick={handleTopicClick} />
       {<FavBadge favouritesCount={favouritesCount} />}
     </div>
